b0a9661eabcbce579285b8c00fa5fb2b839e9bd5
Antonio Olvera <juan.antonio.olvera.cruz@gmail.com>
2020-07-25 19:52:40 -0500

    Updates

---
+++ precache-manifest.de7e5839b56de1ba62b8a4fc0e96230f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90e8484f8aff9e597b3df8a4f88c144",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "9241732d823a8d8f1155",
+    "url": "/monster-app/static/css/main.2b10c6b4.chunk.css"
+  },
+  {
+    "revision": "bb27650c9f5e6e6b07e2",
+    "url": "/monster-app/static/js/2.70e63080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.70e63080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9241732d823a8d8f1155",
+    "url": "/monster-app/static/js/main.d13500b0.chunk.js"
+  },
+  {
+    "revision": "94cbfa6e971df2fb04c8",
+    "url": "/monster-app/static/js/runtime-main.5dc9959b.js"
+  }
+]);
